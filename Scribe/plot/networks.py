39a27be53761f89b75402f8c0a722a9ef2d9b66e
nilsmechtel <nils.mech@googlemail.com>
2020-05-13 17:10:40 +0200

    add source and target gene selection to network plot

--- Scribe/plot/networks.py
+++ Scribe/plot/networks.py
@@ -4,7 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-def vis_causal_net(adata, layout = 'circular', top_n_edges = 10, edge_color = 'gray', figsize=(6, 6), mkdir=None, save=None):
+def vis_causal_net(adata, source_genes = None, target_genes = None, layout = 'circular', top_n_edges = 10,
+                   edge_color = 'gray', node_color = 'skyblue', node_size = 100, figsize= (6, 6),
+                   path = None, save_to = None):
     """Visualize inferred causal regulatory network
 
     This plotting function visualize the inferred causal regulatory network inferred from Scribe.
@@ -42,9 +44,18 @@ def vis_causal_net(adata, layout = 'circular', top_n_edges = 10, edge_color = 'g
     df_mat = df_mat.stack().reset_index()
     df_mat.columns = ['source', 'target', 'weight']
 
+    if source_genes is not None:
+        df_mat = df_mat[[gene in source_genes for gene in df_mat['source']]]
+
+    if target_genes is not None:
+        df_mat = df_mat[[gene in target_genes for gene in df_mat['target']]]
+
     if top_n_edges is not None:
         ind_vec = np.argsort(-df_mat.loc[:, 'weight'])
         df_mat = df_mat.loc[ind_vec[:top_n_edges], :]
+
+    source_genes = df_mat['source'].tolist()
+    target_genes = df_mat['target'].tolist()
 
     G = nx.from_pandas_edgelist(df_mat, source='source', target='target', edge_attr='weight', create_using=nx.DiGraph())
     G.nodes()
@@ -59,93 +70,52 @@ def vis_causal_net(adata, layout = 'circular', top_n_edges = 10, edge_color = 'g
     'arrowsize': 1000,
      }
 
-    plt.figure(figsize=figsize)
-    if layout is None:
-        nx.draw(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+    if type(layout) is not str:
+        pos = layout
+    elif layout is None:
+        pos = None
     elif layout == "circular":
-        nx.draw_circular(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+        pos = nx.circular_layout(G)
     elif layout == "kamada_kawai":
-        nx.draw_kamada_kawai(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+        pos = nx.kamada_kawai_layout(G)
     elif layout == "planar":
-        nx.draw_planar(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+        pos = nx.planar_layout(G)
     elif layout == "random":
-        nx.draw_random(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+        pos = nx.random_layout(G)
     elif layout == "spectral":
-        nx.draw_spectral(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+        pos = nx.spectral_layout(G)
     elif layout == "spring":
-        nx.draw_spring(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+        pos = nx.spring_layout(G)
     elif layout == "shell":
-        nx.draw_shell(G, with_labels=True, node_color='skyblue', node_size=100, edge_color=edge_color, width=W / np.max(W) * 5, edge_cmap=plt.cm.Blues, options = options)
+        pos = nx.shell_layout(G)
     else:
-        raise('layout', layout, ' is not supported.')
+        raise('Layout', layout, ' is not supported.')
 
-    if mkdir is None:
-        path = os.getcwd()
-    else:
-        try:
-            os.mkdir(mkdir)
-        except OSError:
-            print('\n', "Creation of the directory %s failed" % mkdir)
-        else:
-            print('\n', "Successfully created the directory %s" % mkdir)
-        path = mkdir
+    if edge_color == 'weight':
+        edge_color = W
 
-    if save is None:
-        plt.show()
-    else:
-        plt.savefig("%s/%s" % (path,save), dpi=None, facecolor='w', edgecolor='w',
-                    orientation='portrait', papertype=None, format='png',
-                    transparent=False, bbox_inches=None, pad_inches=0.1,
-                    metadata=None)
+    if width == 'weight'
+        width = W / np.max(W) * 5
 
-
-def vis_causal_net2(adata, top_n_edges = 10, node_color='skyblue', figsize=(6, 6), mkdir=None, save=None):
-
-    if 'causal_net' not in adata.uns.keys():
-        raise('causal_net is not a key in uns slot. Please first run causal network inference with Scribe.')
-
-    df_mat = adata.uns['causal_net']
-    ind_mat = np.where(df_mat.values - df_mat.T.values < 0)
-
-    tmp = np.where(df_mat.values - df_mat.T.values < 0)
-
-    for i in range(len(tmp[0])):
-        df_mat.iloc[tmp[0][i], tmp[1][i]] = np.nan
-
-    df_mat = df_mat.stack().reset_index()
-    df_mat.columns = ['source', 'target', 'weight']
-
-    adata.var['gene_name'] = adata.var_names.tolist()
-
-    df_mat.source = adata.var.loc[df_mat.source, 'gene_name'].tolist()
-    df_mat.target = adata.var.loc[df_mat.target, 'gene_name'].tolist()
-
-    df_mat = df_mat.iloc[~np.isinf(df_mat.weight.tolist()), :]
-
-    top_edges = df_mat.weight >= np.quantile(df_mat.weight, 1 - top_n_edges / len(df_mat))
-    df_mat = df_mat.loc[top_edges,]
-
-    G = nx.from_pandas_edgelist(df_mat, source='source', target='target', edge_attr='weight', create_using=nx.DiGraph())
-    G.nodes()
-    W = []
-    for n, nbrs in G.adj.items():
-        for nbr, eattr in nbrs.items():
-            W.append(eattr['weight'])
-
-    G.nodes()
     plt.figure(figsize=figsize)
-    nx.draw(G, with_labels=True, node_color=node_color, node_size=100, edge_color=W, width=1.0, edge_cmap=plt.cm.Blues)
+    nx.draw(G,
+            pos=pos,
+            with_labels=True,
+            node_color=node_color, node_size=node_size,
+            edge_color=edge_color, width=width,
+            edge_cmap=plt.cm.Blues,
+            options = options)
 
-    if mkdir is None:
+    if path is None:
         path = os.getcwd()
     else:
-        try:
-            os.mkdir(mkdir)
-        except OSError:
-            print('\n', "Creation of the directory %s failed" % mkdir)
-        else:
-            print('\n', "Successfully created the directory %s" % mkdir)
-        path = mkdir
+        if not os.isdir(path)
+            try:
+                os.mkdir(mkdir)
+            except OSError:
+                print('\n', "Creation of the directory %s failed" % path)
+            else:
+                print('\n', "Successfully created the directory %s" % path)
 
     if save is None:
         plt.show()
@@ -155,3 +125,60 @@ def vis_causal_net2(adata, top_n_edges = 10, node_color='skyblue', figsize=(6, 6
                     transparent=False, bbox_inches=None, pad_inches=0.1,
                     metadata=None)
 
+    return source_genes, target_genes, pos
+
+
+#def vis_causal_net2(adata, top_n_edges = 10, node_color='skyblue', figsize=(6, 6), mkdir=None, save=None):
+#    if 'causal_net' not in adata.uns.keys():
+#        raise('causal_net is not a key in uns slot. Please first run causal network inference with Scribe.')
+#
+#    df_mat = adata.uns['causal_net']
+#    ind_mat = np.where(df_mat.values - df_mat.T.values < 0)
+#
+#    tmp = np.where(df_mat.values - df_mat.T.values < 0)
+#
+#    for i in range(len(tmp[0])):
+#        df_mat.iloc[tmp[0][i], tmp[1][i]] = np.nan
+#
+#    df_mat = df_mat.stack().reset_index()
+#    df_mat.columns = ['source', 'target', 'weight']
+#
+#    adata.var['gene_name'] = adata.var_names.tolist()
+#
+#    df_mat.source = adata.var.loc[df_mat.source, 'gene_name'].tolist()
+#    df_mat.target = adata.var.loc[df_mat.target, 'gene_name'].tolist()
+#
+#    df_mat = df_mat.iloc[~np.isinf(df_mat.weight.tolist()), :]
+#
+#    top_edges = df_mat.weight >= np.quantile(df_mat.weight, 1 - top_n_edges / len(df_mat))
+#    df_mat = df_mat.loc[top_edges,]
+#
+#    G = nx.from_pandas_edgelist(df_mat, source='source', target='target', edge_attr='weight', create_using=nx.DiGraph())
+#    G.nodes()
+#    W = []
+#    for n, nbrs in G.adj.items():
+#        for nbr, eattr in nbrs.items():
+#            W.append(eattr['weight'])
+#
+#    G.nodes()
+#    plt.figure(figsize=figsize)
+#    nx.draw(G, with_labels=True, node_color=node_color, node_size=100, edge_color=W, width=1.0, edge_cmap=plt.cm.Blues)
+#
+#    if mkdir is None:
+#        path = os.getcwd()
+#    else:
+#        try:
+#            os.mkdir(mkdir)
+#        except OSError:
+#            print('\n', "Creation of the directory %s failed" % mkdir)
+#        else:
+#            print('\n', "Successfully created the directory %s" % mkdir)
+#        path = mkdir
+#
+#    if save is None:
+#        plt.show()
+#    else:
+#        plt.savefig("%s/%s" % (path,save), dpi=None, facecolor='w', edgecolor='w',
+#                    orientation='portrait', papertype=None, format='png',
+#                    transparent=False, bbox_inches=None, pad_inches=0.1,
+#                    metadata=None)
