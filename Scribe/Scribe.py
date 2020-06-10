correct error in CMI calculation

Scribe/Scribe.py
@@ -6,6 +6,8 @@ from .causal_network import cmi
 from multiprocessing import Pool
 
 tmp_input = []
+
+
 def pool_cmi(pos):
     gene_a = tmp_input[pos][0]
     gene_b = tmp_input[pos][1]
@@ -14,8 +16,18 @@ def pool_cmi(pos):
     z = tmp_input[pos][4]
     return (gene_a, gene_b, cmi(x, y, z))
 
-def causal_net_dynamics_coupling(adata, regulator_genes=None, target_genes=None, guide_keys=None, t0_key='spliced', t1_key='velocity', normalize=True, copy=False, number_of_processes=1):
-    """Infer causal networks with dynamics-coupled single cells measurements.
+
+def causal_net_dynamics_coupling(adata,
+                                 TFs=None,
+                                 Targets=None,
+                                 guide_keys=None,
+                                 t0_key='spliced',
+                                 t1_key='velocity',
+                                 normalize=True,
+                                 drop_zero_cells=True,
+                                 copy=False,
+                                 number_of_processes=1):
+"""Infer causal networks with dynamics-coupled single cells measurements.
     Network inference is a insanely challenging problem which has a long history and that none of the existing algorithms work well.
     However, it's quite possible that one or more of the algorithms could work if only they were given enough data. Single-cell
     RNA-seq is exciting because it provides a ton of data. Somewhat surprisingly, just having a lot of single-cell RNA-seq data
@@ -27,18 +39,19 @@ def causal_net_dynamics_coupling(adata, regulator_genes=None, target_genes=None,
     are recently developed that measures the transcriptome of two time points of the same cells. Datasets generated from those methods
     will provide improvements of causal network inference as we comprehensively demonstrated from the manuscript. This function take
     advantages of those datasets to infer the causal networks.
-
     We note that those technological advance may be still not sufficient, radically different methods, for example something like
     highly multiplexed live imaging that can record many genes may be needed.
-
     Arguments
     ---------
     adata: `anndata`
         Annotated data matrix.
-    regulator_genes: `List`
-    target_genes: `List`
+    TFs: `List` or `None` (default: None)
+        The list of transcription factors that will be used for casual network inference.
+    Targets: `List` or `None` (default: None)
+        The list of target genes that will be used for casual network inference.
     guide_keys: `List` (default: None)
-        Whether to scale the expression or velocity values into 0 to 1 before calculating causal networks.
+        The key of the CRISPR-guides, stored as a column in the .obs attribute. This argument is useful
+        for identifying the knockout or knockin genes for a perturb-seq experiment. Currently not used.
     t0_key: `str` (default: spliced)
         Key corresponds to the transcriptome of the initial time point, for example spliced RNAs from RNA velocity, old RNA
         from scSLAM-seq data.
@@ -47,56 +60,75 @@ def causal_net_dynamics_coupling(adata, regulator_genes=None, target_genes=None,
         see Fig 6 of the Scribe preprint) from RNA velocity, old RNA from scSLAM-seq data.
     normalize: `bool`
         Whether to scale the expression or velocity values into 0 to 1 before calculating causal networks.
+    drop_zero_cells: `bool` (Default: True)
+        Whether to drop cells that with zero expression for either the potential regulator or potential target. This
+        can signify the relationship between potential regulators and targets, speed up the calculation, but at the risk
+        of ignoring strong inhibition effects from certain regulators to targets.
     copy: `bool`
         Whether to return a copy of the adata or just update adata in place.
-
     Returns
     ---------
-    An update AnnData object with inferred causal network stored as a matrix related to the key `causal_net` in the `uns` slot.
+        An update AnnData object with inferred causal network stored as a matrix related to the key `causal_net` in the `uns` slot.
     """
 
-    if regulator_genes is None:
-        regulator_genes = adata.var_names.values.tolist()
-    if target_genes is None:
-        target_genes = adata.var_names.values.tolist()
+    if TFs is None:
+        TFs = adata.var_names.tolist()
+    else:
+        TFs = adata.var_names.intersection(TFs).tolist()
+        if len(TFs) == 0:
+            raise Exception(f"The adata object has no gene names from .var_name that intersects with the TFs list you provided")
 
-    genes = list(set(regulator_genes + target_genes))
+    if Targets is None:
+        Targets = adata.var_names.tolist()
+    else:
+        Targets = adata.var_names.intersection(Targets).tolist()
+        if len(Targets) == 0:
+            raise Exception(f"The adata object has no gene names from .var_name that intersect with the Targets list you provided")
 
     if guide_keys is not None:
-        genes = np.unique(adata.obs[guide_keys].tolist())
-        genes = np.setdiff1d(genes, ['*', 'nan', 'neg'])
+        guides = np.unique(adata.obs[guide_keys].tolist())
+        guides = np.setdiff1d(guides, ['*', 'nan', 'neg'])
 
-        idx_var = [vn in genes for vn in adata.var_names]
+        idx_var = [vn in guides for vn in adata.var_names]
         idx_var = np.argwhere(idx_var)
-        genes = adata.var_names.values[idx_var.flatten()].tolist() #[idx_var]
-        regulator_genes = genes
-        target_genes = genes
+        guides = adata.var_names.values[idx_var.flatten()].tolist()
 
     # support sparse matrix:
-    tmp = pd.DataFrame(adata.layers[t0_key].todense()) if isspmatrix(adata.layers[t0_key]) else pd.DataFrame(adata.layers[t0_key])
+    genes = TFs + Targets
+    genes = np.unique(genes)
+    tmp = pd.DataFrame(adata[:, genes].layers[t0_key].todense()) if isspmatrix(adata.layers[t0_key]) \
+        else pd.DataFrame(adata[:, genes].layers[t0_key])
     tmp.index = adata.obs_names
-    tmp.columns = adata.var_names
-    spliced = tmp.loc[:, genes]
-    tmp = pd.DataFrame(adata.layers[t1_key].todense()) if isspmatrix(adata.layers[t1_key]) else pd.DataFrame(adata.layers[t1_key])
+    tmp.columns = adata[:, genes].var_names
+    spliced = tmp
+
+    tmp = pd.DataFrame(adata[:, genes].layers[t1_key].todense()) if isspmatrix(adata.layers[t1_key]) \
+        else pd.DataFrame(adata[:, genes].layers[t1_key])
     tmp.index = adata.obs_names
-    tmp.columns = adata.var_names
-    velocity = tmp.loc[:, genes]
-    velocity[pd.isna(velocity)] = 0 # set NaN value to 0
+    tmp.columns = adata[:, genes].var_names
+    velocity = tmp
+    velocity[pd.isna(velocity)] = 0  # set NaN value to 0
 
     if normalize:
         spliced = (spliced - spliced.mean()) / (spliced.max() - spliced.min())
         velocity = (velocity - velocity.mean()) / (velocity.max() - velocity.min())
 
-    causal_net = pd.DataFrame({node_id: [np.nan for i in regulator_genes] for node_id in target_genes}, index=regulator_genes)
+    #causal_net = pd.DataFrame({node_id: [np.nan for i in regulator_genes] for node_id in target_genes}, index=regulator_genes)
+    causal_net = pd.DataFrame(columns=target_genes, index=regulator_genes)
 
     for g_a in regulator_genes:
         for g_b in target_genes:
             if g_a == g_b:
                 continue
             else:
-                x_orig = spliced.loc[:, g_a].tolist()
-                y_orig = (spliced.loc[:, g_b] + velocity.loc[:, g_b]).tolist() if t1_key is 'velocity' else spliced.loc[:, g_b].tolist()
-                z_orig = velocity.loc[:, g_b].tolist()
+                x_orig = spliced.loc[:, g_a]
+                y_orig = (spliced.loc[:, g_b] + velocity.loc[:, g_b]) if t1_key is 'velocity' else velocity.loc[:, g_b]
+                z_orig = spliced.loc[:, g_b]
+
+                if drop_zero_cells:
+                    xyz_orig = x_orig + y_orig + z_orig
+                    x_orig, y_orig, z_orig = x_orig[xyz_orig > 0].tolist(), y_orig[xyz_orig > 0].tolist(), \
+                                             z_orig[xyz_orig > 0].tolist()
 
                 # input to cmi is a list of list
                 x_orig = [[i] for i in x_orig]
@@ -117,9 +149,5 @@ def causal_net_dynamics_coupling(adata, regulator_genes=None, target_genes=None,
             causal_net.loc[t[0], t[1]] = t[2]
         
     adata.uns['causal_net'] = causal_net
-
-#     logg.info('     done', time = True, end = ' ' if settings.verbosity > 2 else '\n')
-#     logg.hint('perturbation_causal_net \n'
-#               '     matrix is added to adata.uns')
 
     return adata if copy else None

Scribe/plot/networks.py
@@ -6,8 +6,8 @@ import os
 from copy import deepcopy
 
 def vis_causal_net(adata, source_genes = None, target_genes = None, layout = 'circular', top_n_edges = 10,
-                   edge_color = 'gray', width='weight', max_W = None, node_color = 'skyblue', node_size = 100, figsize = (6, 6),
-                   path = None, save_to = None, show = True, fig = None, ax = None):
+                   edge_color = 'gray', width='weight', min_W = None, max_W = None, node_color = 'skyblue', node_size = 100, figsize = (6, 6),
+                   save_to = None, show = True, fig = None, ax = None, face_color='w'):
     """Visualize inferred causal regulatory network
 
     This plotting function visualize the inferred causal regulatory network inferred from Scribe.
@@ -102,24 +102,27 @@ def vis_causal_net(adata, source_genes = None, target_genes = None, layout = 'ci
     else:
         raise('Layout', layout, ' is not supported.')
 
-    if edge_color == 'weight':
-        edge_color = W
+    if min_W is None:
+        min_W = np.min(W)
 
     if max_W is None:
         max_W = np.max(W)
 
+    if edge_color == 'weight':
+        edge_color = W 
+
     if width == 'weight':
-        width = W / max_W * 5
+        width = W / max_W * 8
 
     if fig is None:
         fig, ax = plt.subplots(figsize=figsize)
-    
+     
     nx.draw(G, ax=ax,
             pos=pos,
             with_labels=True,
             node_color=node_color, node_size=node_size,
             edge_color=edge_color, width=width,
-            edge_cmap=plt.cm.Blues,
+            edge_cmap=plt.cm.Blues, edge_vmin=min_W, edge_vmax=max_W, 
             options = options) 
 
     if save_to is not None:
@@ -140,7 +143,7 @@ def vis_causal_net(adata, source_genes = None, target_genes = None, layout = 'ci
                         print('\n', "Successfully created the directory %s" % tmp_path)
 
         plt.ioff()
-        plt.savefig("%s/%s" % (path,save_to), dpi=None, facecolor='w', edgecolor='w',
+        plt.savefig(save_to, dpi=None, facecolor=face_color, edgecolor=face_color,
                     orientation='portrait', papertype=None, format='png',
                     transparent=False, bbox_inches=None, pad_inches=0.1,
                     metadata=None)
@@ -148,5 +151,5 @@ def vis_causal_net(adata, source_genes = None, target_genes = None, layout = 'ci
     if show is True:
         plt.show()
 
-    return (source_genes, target_genes, pos, max_W, fig)
+    return (source_genes, target_genes, pos, (min_W,max_W), fig)
 